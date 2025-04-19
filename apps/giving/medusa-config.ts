7dba954e4a73f0442426fa4d68d7915bca19647f
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-04-19 22:04:14 +0100

    add medusa

---
+++ apps/giving/medusa-config.ts
@@ -0,0 +1,18 @@
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
+
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl:
+      process.env.DATABASE_URL ||
+      "postgres://postgres:Morenee1606@localhost:5432/medusa_db",
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+});
