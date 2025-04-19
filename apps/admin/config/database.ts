0871b1639d84bc6eb2b4ba2b24f6b6caf3865c1c
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-04-19 19:30:24 +0100

    fix bcknd

---
+++ apps/admin/config/database.ts
@@ -0,0 +1,21 @@
+import path from "path";
+
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "church_db"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Morenee1606"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+    pool: {
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
+    },
+    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+  },
+});
