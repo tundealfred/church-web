3fdfd251a591ffb8a10f400803f92fbfc8c950da
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-05-10 16:20:36 +0100

    Fix React build error in 404 page

---
+++ apps/web/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you&apos;re looking for doesn&apos;t exist.</p>
+    </div>
+  );
+}
